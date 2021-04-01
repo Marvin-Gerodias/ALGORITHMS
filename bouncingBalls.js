ad790aecbc894e918f3c572dafb0d4e6a27fec9c
mgabriel29 <marvin.gabriel@gmail.com>
2021-04-01 15:39:41 -0700

    Bouncing Ball Algorithm

--- bouncingBalls.js
+++ bouncingBalls.js
@@ -33,9 +33,7 @@ function bouncingBall(h, bounce, window) {
     if (maxHeight < window) {
         numBounce++;
     }
-    // if(h < 0 || bounce > 0 || bounce < 1 || window > h){
-    //     return false;
-    // }
+
     return numBounce;
 }
 
